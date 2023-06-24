89b9ed6c1456cb86ff930b955633eed143c4e7c3
Nate Kinast <nathan.kinast@greatminds.org>
2023-06-23 20:41:01 -0500

    comments

--- ExtractJS/tkinter-test.py
+++ ExtractJS/tkinter-test.py
@@ -11,7 +11,7 @@ class ExtractApp:
         self.status_text = tk.StringVar()
         self.status_text.set("Status: Waiting for folder selection...")
 
-        # Create "Choose folder" button
+        # "Choose folder" button
         self.choose_folder_button = tk.Button(
             self.root,
             text="Choose folder",
@@ -22,13 +22,13 @@ class ExtractApp:
         )
         self.choose_folder_button.pack(pady=10)
 
-        # Create a label to display the chosen folder
+        # Label to display selected folder
         self.folder_label = tk.Label(
             self.root, textvariable=self.folder_path, bg="#424242", fg="white"
         )
         self.folder_label.pack(pady=10)
 
-        # Create "Extract" button
+        # "Extract" button
         self.extract_button = tk.Button(
             self.root,
             text="Extract",
@@ -38,10 +38,9 @@ class ExtractApp:
             font=("Arial", 10),
         )
         self.extract_button.pack(pady=10)
-        self.extract_button.config(
-            state="disabled"
-        )  # Initially disable the Extract button
+        self.extract_button.config(state="disabled")
 
+        # Text widget to display status
         self.status_text = tk.Text(
             self.root, bg="#424242", fg="white", font=("Arial", 10), width=50, height=10
         )
@@ -49,19 +48,19 @@ class ExtractApp:
         self.append_status("Status: Waiting for folder selection...")
 
     def append_status(self, text):
+        # Append text to the status text widget
         self.status_text.insert(tk.END, text + "\n")
-        self.status_text.see(tk.END)  # Scroll to the end
+        self.status_text.see(tk.END)
 
     def choose_folder(self):
         # Open a folder selection dialog and update the folder_path
         folder_selected = filedialog.askdirectory()
         self.folder_path.set(folder_selected)
         self.append_status("Status: Folder selected. Click 'Extract' to begin.")
-        self.extract_button.config(
-            state="normal"
-        )  # Enable the Extract button after folder selection
+        self.extract_button.config(state="normal")
 
     def getJSFromGGB(self, root_path, fileName):
+        # Extract the JavaScript file from the GGB file
         ggb_file_path = os.path.join(root_path, fileName)
         ggb_file_name = os.path.splitext(ggb_file_path)[0]
         new_js_file_path = ggb_file_name + "-globalJS.js"
@@ -80,6 +79,7 @@ class ExtractApp:
         return True
 
     def extract_function(self):
+        # Extract from all files in the selected folder
         root_path = self.folder_path.get()
         ggb_files = [file for file in os.listdir(root_path) if file.endswith(".ggb")]
 
@@ -94,12 +94,13 @@ class ExtractApp:
                 self.append_status(
                     f"Error occurred while extracting {file}. Error: {str(e)}"
                 )
-                return  # if an error occurs, stop further extraction
+                return
 
         self.append_status("Status: Extraction complete.")
 
 
 def main():
+    # Create the GUI
     root = tk.Tk()
     root.geometry("400x300")
     root.config(bg="#424242")
